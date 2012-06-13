a22b80eb80b4fb203a68ecad25d909f7bf4dec7c
Brendon van Doornum <bvandoornum@msn.com>
2012-06-13 12:58:59 +1000

    Uniting Testing

---
+++ Inception/Inception.Tests/Inceptor_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inception.Tests
+{
+    [TestClass]
+    public class Inceptor_Tests
+    {
+        [TestMethod]
+        public void Inceptor_TestMethod()
+        {
+            var inceptor = new Inceptor();
+            Assert.AreEqual<int>(6, inceptor.TestMethod());
+        }
+    }
+}
---
+++ Inception/Inception/Inceptor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inception
+{
+    public class Inceptor
+    {
+        public int TestMethod()
+        {
+            return 5;
+        }
+    }
+}
